4150438de48886f5e1659ea6abd36471d82f37c6
unknown <rathodsagar9009@gmail.com>
2024-05-21 18:28:52 +0100

    first commit

---
+++ xyz.py
@@ -0,0 +1,17 @@
+# a=int(input("enetr 1 number:"))
+# b=int(input("enetr 2 number:"))
+# c=int(input("enetr 3 number:"))
+# d=int(input("enetr 4 number:"))
+# if(a>b and a>c and a>d):
+#     print("first is a number largest:",a)
+# elif(b>a and b>c and b>d):
+#     print("second is a number largest:",b)
+# elif(c>a and c>b and c>d):
+#     print("third is a number largest",c)
+# else:
+#     print("forth is a number largest",d)
+num=int(input("enetr a nnumber:"))
+if(num % 5==0):
+    print("number is multiple of 5","you enetr number is:",num)
+else:
+    print("number is not multiple of 5","you enetr number is:",num)
