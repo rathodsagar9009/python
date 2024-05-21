4dba8762a4a4bca4fb43583fc9754b72cf37934b
unknown <rathodsagar9009@gmail.com>
2024-05-21 19:22:59 +0100

    first commit

---
+++ listmark.py
@@ -0,0 +1,10 @@
+# mark=[94.4,95.53,63,89]
+# print(mark)
+# print(len(mark))
+# print(mark[0])
+# print(mark[1])
+# student=["sagar",99.5,"padasan"]
+# student[0]="karan"
+# print(student)
+a=[10,20,30,40,50]
+print(a[0:2])
