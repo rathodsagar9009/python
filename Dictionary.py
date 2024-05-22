2235e6fd0d6bcf5d6d1ee947f4691342c3d7d701
unknown <rathodsagar9009@gmail.com>
2024-05-22 16:53:34 +0100

    basic of dictionary

---
+++ Dictionary.py
@@ -0,0 +1,13 @@
+# ama index nathi hota,mutable(change kari kari saki),duplicate nathi thata [list dictionary changing kari saki]
+
+
+#nested dictionary
+student={
+    "name":"sagar",
+    "subject":{
+         "phy":97,
+         "che":95
+
+    }
+}
+print(student["subject"]["phy"])
