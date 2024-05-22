418b9191d3d80292cdcf456fa3cf5c1e91495904
unknown <rathodsagar9009@gmail.com>
2024-05-22 19:08:38 +0100

    dictionary basic

---
+++ set.py
@@ -0,0 +1,14 @@
+# basic of set
+#unoder hoi che,unique,immutable
+#list,dict set ma store nai thake km k aa bane ni value change kari saki,jyare int,float,str,type set ma store thai sake
+collection={1,2,3,4}
+print(type(collection))
+print(collection)
+
+b={1,2,3,3,4,4,5,6,6,"hello","hello"}  # SET ma similire value ne repite nai kare
+print(b)
+
+# sum= set()                      #empty set
+# print(type(sum))
+sum = {}
+print(type(sum))
