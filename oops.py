f249c071812849203734edc7e27d44334c497a21
unknown <rathodsagar9009@gmail.com>
2024-05-24 19:49:04 +0100

    dictionary basic

---
+++ oops.py
@@ -0,0 +1,31 @@
+#  " to map with real world scenarios,we srarted using object in code." obeject oriented programingclas
+# class Student:                  #class
+#     name="arjun"
+
+# s1 = Student()                      #object
+# print(s1.name)  
+# s2 = Student()
+# print(s2.name)   
+
+# class Car:
+#     car="blue"   
+#     model="bmw"
+# c1 = Car()
+# print(c1.car)
+# print(c1.model)
+ 
+
+ # CONSTRUCTOR MEANS KE JETLA OBJECT CREAT THASE TYRE CREAT THASE 
+    # __ININ__ FUNNCTION
+class Student:
+    def __init__(self,name):
+        self.name=name
+        
+
+s1 = Student()
+print(s1) 
+
+
+s2 = Student()
+print(s2) 
+
