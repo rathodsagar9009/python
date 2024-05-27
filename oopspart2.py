3a927aed0e1a88edc32936b66027a45053641c95
unknown <rathodsagar9009@gmail.com>
2024-05-27 19:29:00 +0100

    dictionary basic

--- oopspart2.py
+++ oopspart2.py
@@ -26,17 +26,91 @@
 
 
                                     # PRIVITE ATTRIBUTE & METHOD:-
-class account:
-    def __init__(self,acc_no,acc_pass):
-        self.acc_no=acc_no
-        self.__acc_pass=acc_pass    # " __ koi pan atrribute ni agal double liti laki atle te privite thai jase
+# class account:
+#     def __init__(self,acc_no,acc_pass):
+#         self.acc_no=acc_no
+#         self.__acc_pass=acc_pass    # " __ koi pan atrribute ni agal double liti laki atle te privite thai jase
 
 
-    def reset_pass(self):
-        print(self.__acc_pass)
+#     def reset_pass(self):
+#         print(self.__acc_pass)
 
 
-#object
-s1 = account("312010110002010","5680")   
-print(s1.acc_no)
-print(s1.reset_pass())     
+# #object
+# s1 = account("312010110002010","5680")   
+# print(s1.acc_no)
+# print(s1.reset_pass())     
+
+
+
+
+
+#PILLORE OF OOPS                [  3   INHERITANCE:- CLASS NI AMUK PROPERTIES SAME HOICHE JE BIJA CLASS MA VAPARI SAKI TENE INHERITANCE KEVAY CHE  ]
+
+# #
+# class car: #BASE CLASS
+#     color="black"
+#     @staticmethod
+#     def start():
+#         print("car started")
+
+#     @staticmethod
+#     def stop():
+#         print("car stop")
+
+# class toyotacar(car): #DERIVED CLASS
+#     def __init__(self,name):
+#         self.name=name
+
+#         #object
+
+# car1=toyotacar("Fortuner")
+# car2=toyotacar("pries")
+
+# print(car1.color)
+# print(car1.name)
+
+
+    #INHERITANCE 3 TYPE:-
+    #1]SINGLE INHERITANCE("SINGLE BASE{PARENT CLASS} CLASS ANE SINGLE DERIVED{CHILDE CLASS} CLASS HOI CHE")
+        # UPPER NO EXMPLE CHE
+   
+   
+   
+    #2]MULTI-LEVEL INHERITANCE("SINGLE BASE CLASS ANE ANO DERIVED CLASS HASE ,ANE AA DRIVED CLASS YE BASE CLASS BANI JASE ANA PACHI NO CLASS DERIVED CLASS BANSE ")
+
+# class car:  #BASE CLASS
+#     @staticmethod
+#     def start():
+#         print("car started")
+#     def stop():
+#         print("car stop")
+
+# class toyota(car): #DERIVED CLASS 
+#     def __init__(self,brand):
+#         self.brand=brand
+
+# class fortuner(toyota): #DERIVED CLASS
+#     def __init__(self,type):
+#       self.type = type 
+
+# #object
+# car1=fortuner("deasel")
+# car1.start()
+
+#    3] MULTIPLE INHERITANCE ("BASE CLASS 1 ANE BASE CLASS 2 HASE AND ANO DRIVED CLASS HASE SAME HASE")
+
+class A:# BASE CLASS
+    varA= "wel come to class A"
+class B:# BASE CLASS
+    varB="wel come to class B"    
+class C(A,B):#DRIVED CLASS
+     varC="wel come to class C"    
+    
+
+c1=C()
+print(c1.varC)
+
+
+
+#               [       SUPER METHOD:-PERENT CLASS NI MMETHOD NE ACCESS KARI         ]
